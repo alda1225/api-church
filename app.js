8965be71123c481b9a765f9466122980626e22f7
alda1225 <aldairguzman1225@gmail.com>
2021-01-25 01:06:56 -0400

    app upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const Joi = require('@hapi/joi');
+const movies = require('./movies');
+
+app.use(express.json());
+
+app.use('/abc', movies);
+
+
+app.get('/', (req, res) => {
+    res.send('EN HORA BUENA');
+})
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
